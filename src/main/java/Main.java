3955ffefae430d1191888f0834d1d8b97b287f36
Icaro Rodrigues <icaro.santosdev@gmail.com>
2026-05-08 13:17:42 -0300

    feat: implementação das classes: PasswordConfig, ParserArguments e GeneratorPassword na minha classe Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import generator.GeneratorPassword;
+import parser.ParserArguments;
+import parser.PasswordConfig;
+import picocli.CommandLine;
+
 public class Main {
 
-    public Main(String[] args) {
+    public static void main(String[] args) {
 
+        ParserArguments arguments = new ParserArguments();
+
+        try {
+            CommandLine.populateCommand(arguments, args);
+            PasswordConfig passwordConfig = new PasswordConfig(
+                    arguments.getSize(),
+                    arguments.isHasSymbol(),
+                    arguments.isHasCapital(),
+                    arguments.isHasNumber()
+            );
+
+            GeneratorPassword generatorPassword = new GeneratorPassword(passwordConfig);
+            String password = generatorPassword.generatePassword();
+            System.out.println("Senha Gerada: \033[1;32m" + password + "\033[0m");
+
+        } catch (CommandLine.ParameterException ex) {
+            System.err.println(ex.getMessage());
+            ex.getCommandLine().usage(System.err);
+        }
     }
+
 }
