b15c8c2ec409c62c5be7c1bec79a1bb9b5c858b9
Icaro Rodrigues <icaro.santosdev@gmail.com>
2026-05-08 08:48:02 -0300

    feat: adicionar ParserArguments com mapeamento de Opções com Picocli

--- src/main/java/parser/ParserArguments.java
+++ src/main/java/parser/ParserArguments.java
@@ -1,5 +1,38 @@
 package parser;
 
-public class ParserArguments {
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
 
+
+@Command(name = "generator", mixinStandardHelpOptions = true, description = "Gera senha com base nas configurações que o usuário passa")
+public class ParserArguments {
+    @Option(names = {"--size", "-s", "--tamanho", "-t"}, description = "Tamanho da senha", required = true)
+    private int size;
+
+    @Option(names = {"--hasSymbol", "-hS", "--temSimbolo", "-tS"}, description = "Gostaria de que tenha simbolo? (s/n) ou (y/n)", required = true)
+    private boolean hasSymbol;
+
+    @Option(names = {"--hasCapital", "-hC", "--temMaiuscula", "-tM"}, description = "Gostaria de simbolos? (s/n) ou (y/n)", required = true)
+    private boolean hasCapital;
+
+    @Option(names = {"--hasNumber", "-hN", "--temNumero", "-tN"}, description = "Gostaria de numeros na senha? (s/n) ou (y/n)", required = true)
+    private boolean hasNumber;
+
+   public ParserArguments() {}
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isHasSymbol() {
+        return hasSymbol;
+    }
+
+    public boolean isHasCapital() {
+        return hasCapital;
+    }
+
+    public boolean isHasNumber() {
+        return hasNumber;
+    }
 }
--- src/main/java/parser/PasswordConfig.java
+++ src/main/java/parser/PasswordConfig.java
@@ -1,4 +1,8 @@
 package parser;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 public class PasswordConfig {
+
 }
