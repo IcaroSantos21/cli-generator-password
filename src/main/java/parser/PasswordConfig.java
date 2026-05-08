f2b6b5c6109616b1ddb6d6f25cf2fedfe6c422ad
Icaro Rodrigues <icaro.santosdev@gmail.com>
2026-05-08 08:56:36 -0300

    feat: adicionar PasswordConfig como contêiner de dados para opções analisadas.

--- src/main/java/parser/PasswordConfig.java
+++ src/main/java/parser/PasswordConfig.java
@@ -1,8 +1,31 @@
 package parser;
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 
 public class PasswordConfig {
+    private int size;
+    private boolean hasSymbol;
+    private boolean hasCapital;
+    private boolean hasNumber;
 
+    public PasswordConfig(int size, boolean hasSymbol, boolean hasCapital, boolean hasNumber) {
+        this.size = size;
+        this.hasSymbol = hasSymbol;
+        this.hasCapital = hasCapital;
+        this.hasNumber = hasNumber;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isHasSymbol() {
+        return hasSymbol;
+    }
+
+    public boolean isHasCapital() {
+        return hasCapital;
+    }
+
+    public boolean isHasNumber() {
+        return hasNumber;
+    }
 }
